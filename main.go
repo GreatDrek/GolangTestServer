c58e5eda634696bd21f950281e6211d42565757d
GreatDrek <drek2410@mail.ru>
2018-09-14 23:48:54 +0300

    test

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	s0 := make([]int32, 0, 10)
+
+	for i := 0; i < 10; i++ {
+		s0 = append(s0, rand.Int31())
+	}
+
+	for _, val := range s0 {
+		fmt.Println(val)
+	}
 }
