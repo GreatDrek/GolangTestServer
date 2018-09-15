15187c597266fb0781781d50e904780d5ec8c105
GreatDrek <drek2410@mail.ru>
2018-09-15 17:43:37 +0300

    New update

--- main.go
+++ main.go
@@ -2,43 +2,28 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
-func init() {
-	fmt.Println("test init")
+type allPerson struct {
+	index int
+	person
+}
+
+type person struct {
+	name     string
+	age      byte
+	nickname string
+	key      int
+}
+
+func (ap allPerson) print() {
+	fmt.Println(ap.name)
 }
 
 func main() {
-	One()
-	s0 := make([]int32, 0, 10)
 
-	for i := 0; i < 10; i++ {
-		s0 = append(s0, rand.Int31())
-	}
+	allP := allPerson{1, person{"Drek", 24, "GreatDrek", 2410}}
 
-	for _, val := range s0 {
-		fmt.Println(val)
-	}
+	allP.print()
 
-	p := new(int)
-
-	fmt.Println(p)
-
-	*p = 6
-	// p = 6 // Ошибка, так как p имеет тип *int а не int
-
-	fmt.Println(p, *p)
-}
-
-func One() {
-	fmt.Println("One0")
-	defer Two()
-	fmt.Println("One1")
-}
-
-func Two() {
-	fmt.Println("Two0")
-
-	fmt.Println("Two1")
 }
