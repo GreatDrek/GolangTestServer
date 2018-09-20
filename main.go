abdcfff76129b9e9e28099f02b1ea34838160cc4
GreatDrek <drek2410@mail.ru>
2018-09-20 15:07:49 +0300

    Test chanel interface

--- main.go
+++ main.go
@@ -5,53 +5,32 @@ import (
 	//"time"
 )
 
-var b bool = false
+type myInterface interface {
+	TestFunc()
+}
+
+type myStruct struct {
+	name string
+}
+
+func (mS myStruct) TestFunc() {
+	fmt.Println(mS.name)
+}
 
 func main() {
-	fmt.Println("Start programm")
-	myChan := make(chan string, 3)
-	myInputChan := make(chan string)
-	go inputConsole(myInputChan)
-	go input(myChan, myInputChan)
-	go output(myChan)
+	c := make(chan myInterface)
 
-	for {
-		if b == true {
-			break
-		}
-		//fmt.Println(time.Second)
-		//time.Sleep(time.Second * 5)
-	}
+	go func() {
+		mI := myInterface(myStruct{name: "Drek"})
+		c <- mI
+	}()
+
+	go func() {
+		mI1 := <-c
+		mI1.TestFunc()
+	}()
+
+	fmt.Scanln()
 
 	fmt.Println("End programm")
-}
-
-func inputConsole(c chan string) {
-	var inStr string
-	for {
-		fmt.Scanln(&inStr)
-		if inStr == "exit" {
-			b = true
-			break
-		}
-		c <- inStr
-	}
-}
-
-func input(c chan string, c1 chan string) {
-	for {
-		s := <-c1
-		c <- s
-	}
-}
-
-func output(c chan string) {
-	for {
-		if len(c) == cap(c) {
-			for i := 0; i < cap(c); i++ {
-				fmt.Println("Chanel:", <-c)
-			}
-			//fmt.Println(len(c), cap(c))
-		}
-	}
 }
